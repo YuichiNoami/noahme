fffa55d14d9508ecc238f49aea498f6640909aef
YuichiNoami <ynoami@2xjack.com>
2021-08-29 06:45:40 +0900

    動作確認

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,18 @@
-const mix = require('laravel-mix');
+mix.ts("resources/ts/app.ts", "public/js");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig({
+    resolve: {
+        extensions: [".js", ".jsx", ".vue", ".ts", ".tsx"],
+        alias: {
+            vue$: "vue/dist/vue.esm.js"
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            options: { appendTsSuffixTo: [/\.vue$/] },
+            exclude: /node_modules/
+        }]
+    }
+});
