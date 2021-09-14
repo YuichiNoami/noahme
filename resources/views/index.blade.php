ac2ff2f5d19aabbc5ff891cc3e52c6a41fd318c9
YuichiNoami <ynoami@2xjack.com>
2021-09-15 00:32:53 +0900

    デザイン調整を完了

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,65 +17,56 @@
     <meta data-hid="ogUrl" property="og:url" content="https://noah-me.com/">
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Noto Sans JP:100,300,400,500,700,900|Ubuntu:300,300i,400,400i,500,500i,700,700i|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
-    <link data-hid="appleTouchIcon" rel="apple-touch-icon" type="image/png" href="https://storage.googleapis.com/studio-front/favicon.png">
+    <link data-hid="appleTouchIcon" rel="apple-touch-icon" type="image/png" href="{{ asset('images/apple-touch-icon.png') }}">
     <link rel="canonical" href="https://noah-me.com/">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver%2CMap%2CArray.from"></script>
     <link rel="stylesheet" href="css/app.css">
 </head>
 
 <body>
-    <div id="__nuxt">
+    <div id="__body">
         <div id="__layout">
-            <div data-v-09e81421="" class="container">
-                <div data-v-358dc9fe="" data-v-09e81421="" class="form-messages">
-                    <div data-v-358dc9fe="" class="notifications" style="width: 380px; top: 0px; left: calc(50% - 190px);"><span></span></div>
-                    <div data-v-358dc9fe="" class="notifications" style="width: 380px; top: 0px; left: calc(50% - 190px);"><span></span></div>
-                </div>
-                <div data-v-09e81421="" class="main"><span class="modals"></span>
+            <div class="container">
+                <div class="main">
                     <div class="main-wrap sd">
                         <div class="header-wrap sd">
                             <div class="header-top-space sd"></div>
-                            <div data-s-f3887f55-fe8d-425b-b61d-45162b515304="" class="sd"><i data-s-c6117b98-71c5-407b-988f-2d578d40be3e="" class="icon material-icons link sd">menu</i>
-                                <div data-s-e97b7ad2-b465-481b-b639-73bef08ceeaf="" class="sd"><a href="/" data-s-3f935b99-5b91-4082-8b9a-6498c0db4e9c="" data-r-0_1_1_0_3f935b99-5b91-4082-8b9a-6498c0db4e9c="" class="text link sd link-exact-active link-active">NoahMe</a><a href="/signup" data-s-51aabee1-b2af-4cb0-8643-7f4b4041d157="" data-r-1_1_1_0_51aabee1-b2af-4cb0-8643-7f4b4041d157="" class="text link sd">会員登録</a><a href="/signin" data-s-53fa895a-744d-47cd-9149-c4d4278b3599="" data-r-2_1_1_0_53fa895a-744d-47cd-9149-c4d4278b3599="" class="text link sd">ログイン</a></div><a href="/post" data-s-8221edfd-fce1-4372-b81a-3a9d661b4327="" class="icon material-icons link sd">mode_edit</a><a href="/search" data-s-b019b49e-3c0c-457a-8a06-83453cb29a7e="" class="icon material-icons link sd">search</a>
+                            <div class="header-nav sd"><i class="header-menu icon material-icons link sd">menu</i>
+                                <div class="header-tab sd"><a href="/" class="brand-link text link sd link-exact-active link-active">NoahMe</a>
+                                    <a href="/signup" class="item-link text link sd">会員登録</a>
+                                    <a href="/signin" class="item-link text link sd">ログイン</a>
+                                </div>
+                                <a href="/post" class="item-icon icon material-icons link sd">mode_edit</a>
+                                <a href="/search" class="item-icon icon material-icons link sd">search</a>
                             </div>
-                            <div data-s-f2bdee4f-a410-4837-8435-a659d02b3cca="" class="sd"><a href="/input" data-s-c26afcff-13d3-4fcc-a2a0-e5577c5f8005="" class="link sd">
-                                    <p data-s-e69195b0-802c-4e71-9766-4398a9bb3c13="" data-r-0_0_2_0_e69195b0-802c-4e71-9766-4398a9bb3c13="" class="text sd">インプット</p>
-                                </a><a href="/output" data-s-3b3e7358-274b-4763-a71c-348ba47fe252="" class="link sd">
-                                    <div data-s-d83a941e-139a-4ba4-b17b-8ff80cd1327c="" class="sd">
-                                        <p data-s-cf8462b3-4f12-4b82-800c-2701518111bc="" data-r-0_0_1_2_0_cf8462b3-4f12-4b82-800c-2701518111bc="" class="text sd">アウトプット</p>
-                                    </div>
-                                    <div data-s-b038cd70-2ba3-4a7f-bba2-1d367584ddf1="" class="sd"></div>
-                                </a></div>
+                            <div class="tab-first sd">
+                                <a href="/input" class="tab-link link sd">
+                                    <p class="tab-text text sd">インプット</p>
+                                </a>
+                                <a href="/output" class="tab-link link sd">
+                                    <p class="tab-text text sd">アウトプット</p>
+                                </a>
+                            </div>
                         </div>
                         <div class="cover-image image sd">
-                            <p class="desc-text text sd">インプットとアウトプットを繋げて「まなびあう」新感覚の学習マッチングサービス<br></p>
-                            <div data-s-72b9330e-970e-4186-af24-b7a0f3364706="" class="cover-wrap sd">
+                            <p class="desc-text text sd">インプットとアウトプットを繋げて「まなびあう」新感覚の学習マッチングサービス</p>
+                            <div class="cover-wrap sd">
 
-                                <div class="responsive sd">
-                                    <div data-s-a1b8adb9-35b7-483f-8d2e-e5adb2dab793="" class="sd"><img data-s-bf6a902a-66de-404c-b5e1-5d9e8554e24e="" alt="" src="https://storage.googleapis.com/studio-design-assets/projects/8dO8JVganG/s-2400x561_v-frms_webp_fc436cc1-7c98-4bab-b91d-79025e0e8f68_small.webp" class="sd">
-                                        <p data-s-457a88ac-4a6c-475d-ba5a-ebc9282aa68a="" data-r-1_0_0_0_0_0_0_0_0_1_1_457a88ac-4a6c-475d-ba5a-ebc9282aa68a="" class="text sd">ノアーミー</p>
+                                <div class="logo-left sd">
+                                    <div class="sd"><img alt="" src="{{ asset('images/logotype.png') }}" class="logotype sd">
+                                        <p class="logodesc text sd">ノアーミー</p>
                                     </div>
-                                </div><img data-s-5034114b-1fd5-444e-a19a-de62416a064a="" alt="" src="https://storage.googleapis.com/studio-design-assets/projects/8dO8JVganG/s-1648x2400_v-frms_webp_3747d8ba-6b4e-477e-9a70-4e01013c3163_small.webp" class="sd">
+                                </div>
+                                <img alt="" src="{{ asset('images/logomark.png') }}" class="logomark sd">
                             </div>
                         </div>
                         <div class="about-text sd">
-                            <p data-s-5a631332-887c-4e14-a5e9-7a98c74b414d="" data-r-0_2_5a631332-887c-4e14-a5e9-7a98c74b414d="" class="about-text-body text sd">NoahMe(ノアーミー)とは？</p>
+                            <p class="about-text-body text sd">NoahMe(ノアーミー)とは？</p>
                         </div>
                         <div class="desc-image image sd">
-                            <p class="desc-text-body text sd">「物事を知ることだけでなく人に教えることで、より脳に体系立てて知識を定着できる」<br>という有名な学習メソッドを、ユーザー同士でビデオ通話で実施することでお互いに学びを得るサービスです。<br></p>
-
+                            <p class="desc-text-body text sd">「物事を知ることだけでなく人に教えることで、より脳に体系立てて知識を定着できる」<br>という有名な学習メソッドを、ユーザー同士でビデオ通話で実施することでお互いに学びを得るサービスです。</p>
                         </div>
                     </div>
-                    <div data-v-6d12a702="" class="publish-studio-style">
-
-                    </div>
-
-                    <div data-v-b8181fc6="" class="product-font-style">
-
-                    </div>
-                </div>
-                <div data-v-09e81421="" class="Integrations">
-                    <!---->
                 </div>
             </div>
         </div>
